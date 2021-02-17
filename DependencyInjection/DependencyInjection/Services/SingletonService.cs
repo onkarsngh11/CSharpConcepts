d28f69ecba6633de8534cb3b066586ae394dcd22
a-khanna <akhanna992@gmail.com>
2021-02-17 15:08:56 +0530

    Added dependency injection sample

---
+++ DependencyInjection/DependencyInjection/Services/SingletonService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DependencyInjection.Interfaces;
+
+
+namespace DependencyInjection.Services
+{
+    public class SingletonService : ISingletonService
+    {
+        public Guid Id { get; private set; }
+
+        public SingletonService()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
